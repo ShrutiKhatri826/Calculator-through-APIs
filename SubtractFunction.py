aa816e240526040890d1ccf311ebcbec8f02585a
Shruti Khatri <77100962+ShrutiKhatri826@users.noreply.github.com>
2023-11-13 13:49:21 +0530

    Add files via upload

---
+++ SubtractFunction.py
@@ -0,0 +1,68 @@
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def validate_input(num1, num2):
+    """
+    Validate input parameters num1 and num2, for multiplication.
+
+    Parameters:
+    - num1: First number
+    - num2: Second number
+
+    Returns:
+    - Tuple: (bool, str)
+      - bool: True if validation passes, False otherwise
+      - str: Error message if validation fails
+    """
+    if num1 is None or num2 is None:
+        return False, "Both 'num1' and 'num2' must be provided"
+        
+    if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
+        return False, "Both 'num1' and 'num2' must be numeric values"
+        
+    return True, "Status: 200 OK"
+
+
+def lambda_handler(event, context):
+    try:
+        # Parse the JSON from the 'body' attribute
+        body = json.loads(event['body'])
+        
+        # Extract 'num1' and 'num2' from the body attribute
+        num1 = body.get('num1')
+        num2 = body.get('num2')
+
+        # Validate input
+        validation_result, validation_message = validate_input(num1, num2)
+        if not validation_result:
+            raise ValueError(validation_message)
+
+        # Performing multiplication
+        result = num1 - num2
+
+        # Log the values and result for verification
+        logger.info(f"num1: {num1}, num2: {num2}, result: {result}")
+
+        response = {
+            'statusCode': 200,
+            'body': json.dumps({'result': result})
+        }
+
+    except ValueError as e:
+        response = {
+            'statusCode': 400,
+            'body': json.dumps({'error': str(e)})
+        }
+
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {str(e)}")
+        response = {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal Server Error'})
+        }
+
+    return response
+
